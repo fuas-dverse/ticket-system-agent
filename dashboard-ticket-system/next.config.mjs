7d8f51a881c5e8b39f3ef25a8d0a2dfa5ea58b95
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2024-05-13 11:09:51 +0200

    Added one more security header

--- dashboard-ticket-system/next.config.mjs
+++ dashboard-ticket-system/next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
                 source: '/(.*)', // Use '*' for all routes
                 headers: [
                     {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
                         key: 'Permissions-Policy',
                         value: 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(self), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(self), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(self), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
                     },
