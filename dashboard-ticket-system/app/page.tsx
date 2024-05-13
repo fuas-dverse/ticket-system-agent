ac720353a2257e108d2901a6085801f26f539489
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2024-05-13 11:25:29 +0200

    Added production env

--- dashboard-ticket-system/app/page.tsx
+++ dashboard-ticket-system/app/page.tsx
@@ -8,6 +8,5 @@ import { Label } from "@/components/ui/label"
 export default function Page() {
     return (
         <div></div>
-
     )
 }
--- dashboard-ticket-system/middleware.ts
+++ dashboard-ticket-system/middleware.ts
@@ -1,7 +1,13 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from 'next/server';
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+    // Check if the requested URL is the root
+    if (request.nextUrl.pathname === '/') {
+        // Create a redirect response to the /login page
+        return NextResponse.redirect(process.env.ROOT_URL + '/login');
+    }
+
     return await updateSession(request);
 }
 
