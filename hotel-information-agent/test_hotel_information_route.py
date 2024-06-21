a0eaf4d02cf1515a8b3845ee7821ae07f623460d
Muijsenberg,Reno R.F. <r.muijsenberg@student.fontys.nl>
2024-06-21 10:52:07 +0200

    Fix test to not use client

--- hotel-information-agent/test_hotel_information_route.py
+++ hotel-information-agent/test_hotel_information_route.py
@@ -2,7 +2,7 @@ import pytest
 import requests_mock
 from requests import RequestException
 from init import download_nltk_data
-from app import app, search_hotels
+from app import search_hotels
 
 
 def setup_locations_return_data():
@@ -16,9 +16,12 @@ def setup_locations_return_data():
 def setup_hotel_return_data():
     search_json = {
         "result": [
-            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100", "url": "url1", "main_photo_url": "url1"},
-            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100", "url": "url1", "main_photo_url": "url1"},
-            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100", "url": "url1", "main_photo_url": "url1"}
+            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100",
+             "url": "url1", "main_photo_url": "url1"},
+            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100",
+             "url": "url1", "main_photo_url": "url1"},
+            {"hotel_name": "Hotel1", "address": "Address1", "review_score_word": "Good", "min_total_price": "100",
+             "url": "url1", "main_photo_url": "url1"}
         ]
     }
 
@@ -28,12 +31,6 @@ def setup_hotel_return_data():
 def mock_api_responses(mock_requests, locations_json, search_json):
     mock_requests.get('https://booking-com.p.rapidapi.com/v1/hotels/locations', json=locations_json)
     mock_requests.get('https://booking-com.p.rapidapi.com/v1/hotels/search', json=search_json)
-
-
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        return client
 
 
 @pytest.fixture
@@ -47,23 +44,14 @@ def install_packages():
     download_nltk_data()
 
 
-def test_get_api_endpoint(client, mock_requests):
+def test_get_api_endpoint(mock_requests):
     mock_api_responses(mock_requests, setup_locations_return_data(), setup_hotel_return_data())
-    response = client.get('/I%20want%20to%20book%20a%20hotel%20in%20Spain')
-    assert len(response.get_json()) == 3
-    assert response.status_code == 200
-
-
-def test_post_api_endpoint(client, mock_requests):
-    mock_api_responses(mock_requests, setup_locations_return_data(), setup_hotel_return_data())
-    response = client.post('/I%20want%20to%20book%20a%20hotel%20in%20Spain')
-    assert response.status_code == 405
-    assert b"Method Not Allowed" in response.data
+    result = search_hotels('I want to book a hotel in Spain')
+    assert len(result) == 3
 
 
 def test_search_hotels_returns_hotels_when_city_found(mock_requests):
     mock_api_responses(mock_requests, setup_locations_return_data(), setup_hotel_return_data())
-
     result = search_hotels('I want to book a hotel in Spain')
     assert len(result) == 3
 
@@ -72,14 +60,3 @@ def test_search_hotels_returns_empty_when_no_city_found(mock_requests):
     mock_api_responses(mock_requests, setup_locations_return_data(), setup_hotel_return_data())
     result = search_hotels('I want to book a hotel')
     assert result is None
-
-
-def mock_get(*args, **kwargs):
-    raise RequestException
-
-
-def test_request_exception(monkeypatch, mock_requests):
-    mock_api_responses(mock_requests, setup_locations_return_data(), setup_hotel_return_data())
-    monkeypatch.setattr("requests.get", mock_get)
-    result = search_hotels("Amsterdam")
-    assert result == 'An error occurred: '
